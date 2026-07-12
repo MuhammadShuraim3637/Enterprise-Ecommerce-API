1ddf35ffbe5d88ad3758b756f1e390910fdf84ed
MuhammadShuraim3637 <shuraimbutt4@gmail.com>
2026-07-12 21:24:41 +0500

    fix main

--- app/main.py
+++ app/main.py
@@ -1,3 +1,9 @@
+
+import logging
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s"
+)
 from typing import Dict
 from pathlib import Path
 
